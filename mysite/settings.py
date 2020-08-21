c40ad7104f57d0db17b73f3c84b5aec2f1f14a43
lakshyatm <58063296+lakshyatm@users.noreply.github.com>
2020-08-21 19:10:23 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kriyablog.herokuapp.com']
+ALLOWED_HOSTS = ['https://powerful-fortress-96203.herokuapp.com/']
 
 
 # Application definition
